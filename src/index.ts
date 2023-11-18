3d13f26b280c2d67c6d773d3bff7b142a9e1f331
Francisco Grande <fjgrandv@gmail.com>
2023-11-18 18:06:51 +0100

    Fix env port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import chalk from "chalk";
 import { connectToDatabase } from "./database/index.js";
 import { startServer } from "./server/app.js";
 
-const port = process.env.POR ?? 4000;
+const port = process.env.PORT ?? 4000;
 
 if (!process.env.MONGODB_URL) {
   console.log(chalk.red("Missing connection database"));
