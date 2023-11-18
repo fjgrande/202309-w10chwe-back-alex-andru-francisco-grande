c4cc982c3a3708a47cccfb835824a403548bcab1
Francisco Grande <fjgrandv@gmail.com>
2023-11-18 17:47:14 +0100

    Add connected to database and port

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-// Director de orquesta 🦥
+import chalk from "chalk";
+import { connectToDatabase } from "./database";
+import { startServer } from "./server/app";
+
+const port = process.env.POR ?? 4000;
+
+if (!process.env.MONGODB_URL) {
+  console.log(chalk.red("Missing connection database"));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
+startServer(+port);
