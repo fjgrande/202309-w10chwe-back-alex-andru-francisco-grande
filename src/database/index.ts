1ce3263daee563895d746edda6e43f6a3566d605
Francisco Grande <fjgrandv@gmail.com>
2023-11-18 17:18:50 +0100

    Add mensage conected database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,12 @@
+import chalk from "chalk";
 import mongoose from "mongoose";
 
 export const connectToDatabase = async (mongoUrl: string) => {
   try {
     await mongoose.connect(mongoUrl);
     mongoose.set("debug", true);
-    // Mensaje para cuando se ha conectado 🐼
+    console.log(chalk.blue("Connected to databse"));
   } catch (error) {
-    // Mensaje para cuando no se ha conectado 🐇
+    console.log(chalk.red("Falided connecting to database"));
   }
 };
