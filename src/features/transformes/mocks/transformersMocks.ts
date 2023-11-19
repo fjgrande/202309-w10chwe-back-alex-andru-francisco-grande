4edc9cd7e1ea7e08d16bf7a696a7a84a30cad582
Francisco Grande <fjgrandv@gmail.com>
2023-11-19 15:35:10 +0100

    Add transformersMocks

---
+++ src/features/transformes/mocks/transformersMocks.ts
@@ -0,0 +1,20 @@
+const transfomersMocks = [
+  {
+    name: "Optimus Prime",
+    faction: "Autobot",
+    vehicleMode: "Peterbilt truck",
+    imageUrl: "https://i.ibb.co/cbzKq9t/Optimus-Prime.webp",
+    speed: 5,
+    resistance: 7,
+  },
+  {
+    name: "Megatron",
+    faction: "Decepticon",
+    vehicleMode: "Cybertronian tank",
+    imageUrl: "https://i.ibb.co/D7rjM6Y/Megatron-Art.webp",
+    speed: 10,
+    resistance: 2,
+  },
+];
+
+export default transfomersMocks;
