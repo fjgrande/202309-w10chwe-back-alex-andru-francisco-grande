e679f3ab2d928248594eaa9f63c5ccd9e0abc85e
Francisco Grande <fjgrandv@gmail.com>
2023-11-21 10:04:20 +0100

    Add userRouter

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,12 +5,13 @@ import cors from "cors";
 import { notFound } from "./middlewares/errors/errorMiddleware.js";
 import pingRouter from "../features/ping/router/pingRouter.js";
 import transformersRouter from "../features/transformes/router/transformersRouter.js";
+import { userRouter } from "../features/user/router/userRouter.js";
 
 app.use(morgan("dev"));
 app.use(cors({ origin: "*" }));
+app.use(express.json());
 
 app.use("/transformers", transformersRouter);
 app.use("/", pingRouter);
+app.use("/", userRouter);
 app.use(notFound);
-
-app.use(express.json());
